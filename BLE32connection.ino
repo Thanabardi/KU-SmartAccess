8e1d34713b61aea5719882850d3d05abe3751ebd
Kittison Jackthreemongkol <78125325+touchtool@users.noreply.github.com>
2024-02-10 14:44:18 +0700

    Create FIFO in order to receive message from mobile device

--- BLE32connection.ino
+++ BLE32connection.ino
@@ -10,9 +10,15 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 int txValue = 0;
 
+#define MAX_BUFFER_SIZE 100
+#define MAX_STRING_LENGTH 20
 #define SERVICE_UUID "1E200001-B4A5-F678-E9A0-E12E34DCCA5E"
 #define CHARACTERISTIC_UUID_RX "1E200002-B4A5-F678-E9A0-E12E34DCCA5E"
 #define CHARACTERISTIC_UUID_TX "1E200003-B4A5-F678-E9A0-E12E34DCCA5E"
+
+char fifoBuffer[MAX_BUFFER_SIZE][MAX_STRING_LENGTH];
+int fifoHead = 0;
+int fifoTail = 0;
 
 BLECharacteristic doorCharacteristic(
     BLEUUID((uint16_t)0x2A6E),
@@ -55,6 +61,45 @@ void checkToReconnect() {
   }
 }
 
+void addToBuffer(const std::string& str) {
+    if (((fifoTail + 1) % MAX_BUFFER_SIZE) != fifoHead) { // Check for buffer full
+        str.copy(fifoBuffer[fifoTail], MAX_STRING_LENGTH);
+        fifoTail = (fifoTail + 1) % MAX_BUFFER_SIZE;
+    } else {
+        // Handle buffer overflow
+        Serial.println("Buffer overflow!");
+    }
+}
+
+std::string removeFromBuffer() {
+    std::string str;
+    if (fifoHead != fifoTail) { // Check for buffer empty
+        str = fifoBuffer[fifoHead];
+        fifoHead = (fifoHead + 1) % MAX_BUFFER_SIZE;
+    } else {
+        // Handle buffer underflow
+        Serial.println("Buffer underflow!");
+    }
+    return str;
+}
+
+void processBuffer() {
+    while (fifoHead != fifoTail) {
+        std::string str = removeFromBuffer();
+        // Process the string
+        Serial.print("Received: ");
+        Serial.println(str.c_str());
+    }
+}
+
+// Callback invoked when data is written to the BLE characteristic
+class CharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+        std::string value = pCharacteristic->getValue();
+        addToBuffer(value);
+    }
+};
+
 void setup() {
   Serial.begin(9600);
 
@@ -87,6 +132,9 @@ void setup() {
           BLECharacteristic::PROPERTY_WRITE
   );
 
+  // Set the callback for the characteristic
+  pRxCharacteristic->setCallbacks(new CharacteristicCallbacks());
+
   pServer->getAdvertising()->addServiceUUID(pService->getUUID());
 
   // start service
@@ -116,10 +164,7 @@ void loop() {
     Serial.println("Device: " + String(deviceConnected) + "\n");
 
     // read message
-    std::string value = pRxCharacteristic->getValue().c_str();
-    if (!value.empty()) {
-      Serial.print("Received: " + String(value.c_str()));
-    }
+    processBuffer();
 
     delay(500);
   }
